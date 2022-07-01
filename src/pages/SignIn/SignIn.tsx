98f2dc02a3a1345a79a13bf9b0871f73da62a043
lamhoang1256 <lamhoang1256@gmail.com>
2022-07-01 14:58:03 +0700

    Feat: config firebase

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -2,18 +2,18 @@ import shareVideo from "assets/videos/share.mp4";
 
 const SignIn = () => {
   return (
-    <div className='h-screen w-full relative'>
+    <div className="h-screen w-full relative">
       <video
         src={shareVideo}
         loop
         controls={false}
         muted
         autoPlay
-        className='w-full h-full object-cover'
+        className="w-full h-full object-cover"
       />
-      <div className='absolute inset-0 flex flex-col items-center justify-center gap-y-5'>
-        <h2 className='text-white'>Welcome to Pinterjoy</h2>
-        <button type='button' className='bg-rede7 text-white rounded-xl block px-10 py-5'>
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-5">
+        <h2 className="text-white">Welcome to Pinterjoy</h2>
+        <button type="button" className="bg-rede7 text-white rounded-xl block px-10 py-5">
           Sign In With Google
         </button>
       </div>
